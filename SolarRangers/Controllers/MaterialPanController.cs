88707e0e3ff9e4775e76a16333f514ff9fead5a9
Insmoshable <MiJSchloeder@gmail.com>
2024-03-25 20:00:48 -0500

    Create MaterialPanController.cs
    Made simple script to pan materials on object

---
+++ SolarRangers/Controllers/MaterialPanController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace SolarRangers.Controllers
+{
+    internal class MaterialPanController : MonoBehaviour
+    {
+        Material materialRef;
+        public int materialIndex;
+        public Vector2 scrollSpeed;
+        void Awake() {
+            materialRef = GetComponent<MeshRenderer>().sharedMaterials[materialIndex];
+        }
+        public void Update()
+        {
+            materialRef.mainTextureOffset += scrollSpeed * Time.deltaTime;
+        }
+    }
+}
